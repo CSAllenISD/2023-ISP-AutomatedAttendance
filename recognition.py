2cc0641262a3145964d56465fb01844922eb3b88
Cameron Farley <camfarley27@icloud.com>
2023-01-20 23:29:59 -0600

    updated comments

--- recognition.py
+++ recognition.py
@@ -1,4 +1,4 @@
-# pip3 install: opencv-python, dlib==19.22, and face_recognition
+# pip3 install: opencv-python, dlib, and face_recognition
 import face_recognition
 import os
 import sys
@@ -48,26 +48,21 @@ class FaceRecognition:
         while True:
             ret, frame = video_capture.read()
 
-            # Only process every other frame of video to save time
             if self.process_current_frame:
-                # Resize frame of video to 1/4 size for faster face recognition processing
                 small_frame = cv2.resize(frame, (0, 0), fx=0.25, fy=0.25)
 
-                # Convert the image from BGR color (which OpenCV uses) to RGB color (which face_recognition uses)
+                #change from bgr to rgb
                 rgb_small_frame = small_frame[:, :, ::-1]
 
-                # Find all the faces and face encodings in the current frame of video
                 self.face_locations = face_recognition.face_locations(rgb_small_frame)
                 self.face_encodings = face_recognition.face_encodings(rgb_small_frame, self.face_locations)
 
                 self.face_names = []
                 for face_encoding in self.face_encodings:
-                    # See if the face is a match for the known face(s)
                     matches = face_recognition.compare_faces(self.known_face_encodings, face_encoding)
                     name = "Unknown"
                     confidence = '???'
 
-                    # Calculate the shortest distance to face
                     face_distances = face_recognition.face_distance(self.known_face_encodings, face_encoding)
 
                     best_match_index = np.argmin(face_distances)
@@ -79,27 +74,23 @@ class FaceRecognition:
 
             self.process_current_frame = not self.process_current_frame
 
-            # Display the results
+            #results
             for (top, right, bottom, left), name in zip(self.face_locations, self.face_names):
-                # Scale back up face locations since the frame we detected in was scaled to 1/4 size
                 top *= 4
                 right *= 4
                 bottom *= 4
                 left *= 4
 
-                # Create the frame with the name
                 cv2.rectangle(frame, (left, top), (right, bottom), (0, 0, 255), 2)
                 cv2.rectangle(frame, (left, bottom - 35), (right, bottom), (0, 0, 255), cv2.FILLED)
                 cv2.putText(frame, name, (left + 6, bottom - 6), cv2.FONT_HERSHEY_DUPLEX, 0.8, (255, 255, 255), 1)
 
-            # Display the resulting image
+            #display
             cv2.imshow('Face Recognition', frame)
 
-            # Hit 'q' on the keyboard to quit!
             if cv2.waitKey(1) == ord('q'):
                 break
 
-        # Release handle to the webcam
         video_capture.release()
         cv2.destroyAllWindows()
 
