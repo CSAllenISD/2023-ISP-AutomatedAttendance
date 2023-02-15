c169e7d3db828298e1755007ec828a019818692f
William Clymire <70544799+Bread21234@users.noreply.github.com>
2023-02-15 14:59:39 -0600

    building base api

---
+++ api1.py
@@ -0,0 +1,19 @@
+#api1.py
+
+from flask import Flask
+
+# Creating a new "app" by using the Flask constructor. Passes __name__ as a parameter.
+app = Flask(__name__)
+
+
+in_memory_datastore = {
+    "Cameron" : {"name": "Cameron", "eye-color": "Brown" },
+    "Oliver" : {"name": "Oliver", "eye-color": "Blue"},
+    "Connor" : {"name": "Connor", "eye-color": "Brown"}   
+}
+
+@app.get('/students_in_school')
+def students_in_school():
+    return {"students_in_school":list(in_memory_datastore.values())}
+
+
