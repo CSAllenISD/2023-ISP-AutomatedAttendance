47f839efca9659afe77f89b81f623bbacd00b3f7
William Clymire <70544799+Bread21234@users.noreply.github.com>
2023-03-13 15:25:05 -0500

    fixed a small error with blod data

--- database/readBlob.py
+++ database/readBlob.py
@@ -25,9 +25,9 @@ def readBlobData(empId):
          #   photoPath = "E:\pynative\Python\photos\db_data\\" + name + ".jpg"
          #   resumePath = "E:\pynative\Python\photos\db_data\\" + name + ".txt"
             photoPath =  name + ".jpg"
-            #resumePath ="photos/" + name + ".txt"
+            resumePath = name + ".txt"
             writeTofile(photo, photoPath)
-          #  writeTofile(resumeFile, resumePath)
+        writeTofile(resumeFile, resumePath)
 
         cursor.close()
 
@@ -38,37 +38,38 @@ def readBlobData(empId):
             sqliteConnection.close()
             print("sqlite connection is closed")
 # Get the data based on id
-# readBlobData(2) 
+readBlobData(2) 
 
-def readBlobData(empId):
-    try:
-        sqliteConnection = sqlite3.connect('User1.db') #specific database name
-        cursor = sqliteConnection.cursor()
-        print("Connected to SQLite")
+# experiement to get COUNT func
+# def readBlobData(empId):
+#     try:
+#         sqliteConnection = sqlite3.connect('User1.db') #specific database name
+#         cursor = sqliteConnection.cursor()
+#         print("Connected to SQLite")
 
-        sql_fetch_blob_query = """SELECT * from new_employee where id = ?"""
-        cursor.execute(sql_fetch_blob_query, (empId,))
-        record = cursor.fetchall()
-        for row in record:
-            print("Id = ", row[0], "Name = ", row[1])
-            name = row[1]
-            photo = row[2]
-            resumeFile = row[3]
+#         sql_fetch_blob_query = """SELECT * from new_employee where id = ?"""
+#         cursor.execute(sql_fetch_blob_query, (empId,))
+#         record = cursor.fetchall()
+#         for row in record:
+#             print("Id = ", row[0], "Name = ", row[1])
+#             name = row[1]
+#             photo = row[2]
+#             resumeFile = row[3]
 
-            print("Storing employee image and resume on disk \n")
-         #   photoPath = "E:\pynative\Python\photos\db_data\\" + name + ".jpg"
-         #   resumePath = "E:\pynative\Python\photos\db_data\\" + name + ".txt"
-            photoPath =  name + ".jpg"
-            #resumePath ="photos/" + name + ".txt"
-            writeTofile(photo, photoPath)
-          #  writeTofile(resumeFile, resumePath)
+#             print("Storing employee image and resume on disk \n")
+#          #   photoPath = "E:\pynative\Python\photos\db_data\\" + name + ".jpg"
+#          #   resumePath = "E:\pynative\Python\photos\db_data\\" + name + ".txt"
+#             photoPath =  name + ".jpg"
+#             #resumePath ="photos/" + name + ".txt"
+#             writeTofile(photo, photoPath)
+#           #  writeTofile(resumeFile, resumePath)
 
-        cursor.close()
+#         cursor.close()
 
-    except sqlite3.Error as error:
-        print("Failed to read blob data from sqlite table", error)
-    finally:
-        if sqliteConnection:
-            sqliteConnection.close()
-            print("sqlite connection is closed")
+#     except sqlite3.Error as error:
+#         print("Failed to read blob data from sqlite table", error)
+#     finally:
+#         if sqliteConnection:
+#             sqliteConnection.close()
+#             print("sqlite connection is closed")
 
