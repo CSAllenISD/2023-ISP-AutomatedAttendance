354830fce29a913ce029380e69f920329618c2cf
William Clymire <70544799+Bread21234@users.noreply.github.com>
2023-03-23 23:30:57 -0500

    flask first tests->displays on website

--- database/readBlob.py
+++ database/readBlob.py
@@ -1,5 +1,6 @@
 import sqlite3
 import os.path
+import json
 
 def writeTofile(data, filename):
     # Convert binary data to proper format and write it on Hard Disk
@@ -9,7 +10,7 @@ def writeTofile(data, filename):
 
 def readBlobData(empId):
     try:
-        sqliteConnection = sqlite3.connect('User1.db') #specific database name
+        sqliteConnection = sqlite3.connect('database/User1.db') #specific database name
         cursor = sqliteConnection.cursor()
         print("Connected to SQLite")
 
@@ -25,12 +26,10 @@ def readBlobData(empId):
             print("Storing student image and resume on disk \n")
          #   photoPath = "E:\pynative\Python\photos\db_data\\" + name + ".jpg"
          #   resumePath = "E:\pynative\Python\photos\db_data\\" + name + ".txt"
-            photoPath =  name + ".jpg"
-            photoFolder = 'photos/'
-            completePhotoName = os.path.join(photoFolder, photoPath)
+            photoPath =  "photos" + name + ".jpg"
             resumePath = name + ".txt"
 
-            writeTofile(photo, completePhotoName)
+            writeTofile(photo, photoPath)
             writeTofile(resumeFile, resumePath)
 
         cursor.close()
@@ -42,38 +41,6 @@ def readBlobData(empId):
             sqliteConnection.close()
             print("sqlite connection is closed")
 # Get the data based on id
-readBlobData(2) 
+json.dumps(readBlobData(2))
 
-# experiement to get COUNT func
-# def readBlobData(empId):
-#     try:
-#         sqliteConnection = sqlite3.connect('User1.db') #specific database name
-#         cursor = sqliteConnection.cursor()
-#         print("Connected to SQLite")
-
-#         sql_fetch_blob_query = """SELECT * from User1 where id = ?"""
-#         cursor.execute(sql_fetch_blob_query, (empId,))
-#         record = cursor.fetchall()
-#         for row in record:
-#             print("Id = ", row[0], "Name = ", row[1])
-#             name = row[1]
-#             photo = row[2]
-#             resumeFile = row[3]
-
-#             print("Storing employee image and resume on disk \n")
-#          #   photoPath = "E:\pynative\Python\photos\db_data\\" + name + ".jpg"
-#          #   resumePath = "E:\pynative\Python\photos\db_data\\" + name + ".txt"
-#             photoPath =  name + ".jpg"
-#             #resumePath ="photos/" + name + ".txt"
-#             writeTofile(photo, photoPath)
-#           #  writeTofile(resumeFile, resumePath)
-
-#         cursor.close()
-
-#     except sqlite3.Error as error:
-#         print("Failed to read blob data from sqlite table", error)
-#     finally:
-#         if sqliteConnection:
-#             sqliteConnection.close()
-#             print("sqlite connection is closed")
 
--- database/ugh.py
+++ database/ugh.py
@@ -1,6 +1,6 @@
 import sqlite3
 
-conn = sqlite3.connect('User1.db')
+conn = sqlite3.connect('database/User1.db')
 c = conn.cursor()
 #  ?creates new table
 # c.execute("""CREATE TABLE new_employee(
@@ -12,18 +12,19 @@ c = conn.cursor()
 
 def convertToBinaryData(filename):
     # Convert digital data to binary format
+    # 
     with open(filename, 'rb') as file:
         blobData = file.read()
     return blobData
 
 def insertBLOB(empId, name, photo, resumeFile):
     try:
-        sqliteConnection = sqlite3.connect('User1.db')
+        sqliteConnection = sqlite3.connect('database/User1.db')
         cursor = sqliteConnection.cursor()
         print("Connected to SQLite")
         sqlite_insert_blob_query = """ INSERT INTO new_employee
                                   (id, name, photo, resume) VALUES (?, ?, ?, ?)"""
-
+    # error is here
         empPhoto = convertToBinaryData(photo)
         resume = convertToBinaryData(resumeFile)
         # Convert data into tuple format
@@ -41,4 +42,4 @@ def insertBLOB(empId, name, photo, resumeFile):
             print("the sqlite connection is closed")
 
 # insertBLOB(1, "Smith", "photos/Smith.jpg", "photos/random.txt")
-insertBLOB(2,"Connor", "photos/Smith.jpg", "photos/random.txt")
+insertBLOB(2,"Connor", "database/photos/Ayaan.jpg", "database/photos/random.txt")
