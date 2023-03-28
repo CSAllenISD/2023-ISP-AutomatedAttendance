3c6d3b389c9fa8203f9e61a4fbe462d948217720
Vijay Vemulapalli <vvijay1850@gmail.com>
2023-03-28 18:55:36 -0500

    finished transfer learning facial recognition AI

--- transferlearning.py
+++ transferlearning.py
@@ -45,3 +45,31 @@ new_model.fit_generator(
 
 # Step 8: Save the model
 new_model.save('facial_recognition_model.h5')
+
+# Step 9: Use the model to recognize faces
+face_cascade = cv2.CascadeClassifier('haarcascade_frontalface_default.xml')
+model = keras.models.load_model('facial_recognition_model.h5')
+
+cap = cv2.VideoCapture(0)
+
+while True:
+    ret, frame = cap.read()
+    gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+    faces = face_cascade.detectMultiScale(gray, 1.3, 5)
+    for (x,y,w,h) in faces:
+        roi_gray = gray[y:y+h, x:x+w]
+        roi_gray = cv2.resize(roi_gray, (224, 224))
+        roi_gray = np.reshape(roi_gray, (1, 224, 224, 1))
+        prediction = model.predict(roi_gray)
+        if np.argmax(prediction) == 0:
+            cv2.rectangle(frame,(x,y),(x+w,y+h),(255,0,0),2)
+            cv2.putText(frame, 'John Doe', (x, y), cv2.FONT_HERSHEY_SIMPLEX, 1, (255, 0, 0), 2)
+        else:
+            cv2.rectangle(frame,(x,y),(x+w,y+h),(0,0,255),2)
+            cv2.putText(frame, 'Jane Doe', (x, y), cv2.FONT_HERSHEY_SIMPLEX, 1, (0, 0, 255), 2)
+    cv2.imshow('Facial Recognition AI', frame)
+    if cv2.waitKey(1) & 0xFF == ord('q'):
+        break
+
+cap.release()
+cv2.destroyAllWindows()
