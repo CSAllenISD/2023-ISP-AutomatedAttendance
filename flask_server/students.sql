55286a36158b04c0a1c709809f295d37f0327a00
William Clymire <70544799+Bread21234@users.noreply.github.com>
2023-04-13 22:58:06 -0500

    Using time to display dif periods

---
+++ flask_server/students.sql
@@ -0,0 +1,22 @@
+-- (A) USERS
+CREATE TABLE `students` (
+  `user_id` INTEGER NOT NULL,
+  `user_name` TEXT NOT NULL,
+  `user_email` TEXT DEFAULT NULL,
+  PRIMARY KEY("user_id" AUTOINCREMENT)
+);
+ 
+-- (B) DUMMY USERS
+INSERT INTO `students`
+  (`user_name`, `user_email`)
+VALUES
+  ("Jo Doe", "jo@doe.com"),
+  ("Job Doe", "job@doe.com"),
+  ("Joe Doe", "joe@doe.com"),
+  ("Jog Doe", "jog@doe.com"),
+  ("Joi Doe", "joi@doe.com"),
+  ("Jol Doe", "jol@doe.com"),
+  ("Jon Doe", "jon@doe.com"),
+  ("Jos Doe", "jos@doe.com"),
+  ("Jou Doe", "jou@doe.com"),
+  ("Joy Doe", "joy@doe.com");
