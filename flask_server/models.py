cc953f212153894e28c480654b0052889953190f
OliverHankins <oliver.hankins@student.allenisd.org>
2023-04-17 01:21:17 -0500

    merging frontend and development website

--- flask_server/models.py
+++ flask_server/models.py
@@ -3,11 +3,11 @@ from flask_login import UserMixin
 from sqlalchemy.sql import func
 
 
-class Note(db.Model):
-    id = db.Column(db.Integer, primary_key=True)
-    data = db.Column(db.String(10000))
-    date = db.Column(db.DateTime(timezone=True), default=func.now())
-    user_id = db.Column(db.Integer, db.ForeignKey('user.id'))
+#class Note(db.Model):
+ #   id = db.Column(db.Integer, primary_key=True)
+  #  data = db.Column(db.String(10000))
+   # date = db.Column(db.DateTime(timezone=True), default=func.now())
+    #user_id = db.Column(db.Integer, db.ForeignKey('user.id'))
 
 #class classPeriod(db.Model):
  #   id = db.Column(db.Integer, primary_key=True)
