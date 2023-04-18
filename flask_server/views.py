98f44efe4fdf65b00247bae9b21ab84bf1da1231
oliver.hankins <oliver.hankins@gmail.com>
2023-04-18 14:55:28 -0500

    branching off to another branch

--- flask_server/views.py
+++ flask_server/views.py
@@ -1,6 +1,5 @@
 from flask import Blueprint, render_template, request, flash, jsonify
 from flask_login import login_required, current_user
-from .models import Note
 from . import db
 import json
 
