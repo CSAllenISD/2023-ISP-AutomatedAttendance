cf1ce35ec2b1ebfc3dc0effdd0e66820bd514595
Cameron Farley <camfarley27@icloud.com>
2023-04-18 15:26:56 -0500

    finished filter function and student count

--- add-student.js
+++ add-student.js
@@ -3,6 +3,11 @@ var btn = document.getElementById("myBtn");
 var span = document.getElementsByClassName("close")[0];
 var modalBtn = document.getElementsByClassName("modal__btn")[0];
 
+function studentCount() {
+    var table = document.getElementById("tableID").rows.length - 1;
+    document.getElementById("countDisplay").innerHTML = table + " students";
+}
+
 btn.onclick = function() {
     modal.style.display = "block";
 }
@@ -18,5 +23,24 @@ modalBtn.onclick = function() {
 window.onclick = function(event) {
     if (event.target == modal) {
       modal.style.display = "none";
+    }
+}
+
+function filterFunction() {
+    var input, filter, table, tr, td, i, txtValue;
+    input = document.getElementById("myInput");
+    filter = input.value.toUpperCase();
+    table = document.getElementById("tableID");
+    tr = table.getElementsByTagName("tr");
+    for (i = 0; i < tr.length; i++) {
+      td = tr[i].getElementsByTagName("td")[0];
+      if (td) {
+        txtValue = td.textContent || td.innerText;
+        if (txtValue.toUpperCase().indexOf(filter) > -1) {
+          tr[i].style.display = "";
+        } else {
+          tr[i].style.display = "none";
+        }
+      }       
     }
 }
