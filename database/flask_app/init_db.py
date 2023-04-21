dcfb67090a724635b6a56c83989b63e43540c919
William Clymire <70544799+Bread21234@users.noreply.github.com>
2023-04-21 02:41:09 -0500

    buggy facial rec crashes

---
+++ database/flask_app/init_db.py
@@ -0,0 +1,20 @@
+import sqlite3
+# edit which file it reads from later
+connection = sqlite3.connect('database2.db')
+
+
+with open('schema.sql') as f:
+    connection.executescript(f.read())
+
+cur = connection.cursor()
+
+cur.execute("INSERT INTO posts (title, content) VALUES (?, ?)",
+            ('First Post', 'Content for the first post')
+            )
+
+cur.execute("INSERT INTO posts (title, content) VALUES (?, ?)",
+            ('Second Post', 'Content for the second post')
+            )
+
+connection.commit()
+connection.close()
--- flask_server/createTable.py
+++ flask_server/createTable.py
@@ -1,5 +1,7 @@
+# manuel use file currently
 # (A) LOAD PACKAGES
-import sqlite3, os
+import sqlite3
+import os
 from sqlite3 import Error
 
 # (B) DATABASE + SQL FILE
@@ -8,12 +10,12 @@ SQLFILE = "allClasses.sql"
 
 # (C) DELETE OLD DATABASE IF EXIST
 if os.path.exists(DBFILE):
-  os.remove(DBFILE)
- 
+    os.remove(DBFILE)
+
 # (D) IMPORT SQL
 conn = sqlite3.connect(DBFILE)
 with open(SQLFILE) as f:
-  conn.executescript(f.read())
+    conn.executescript(f.read())
 conn.commit()
 conn.close()
 print("Database created!")
