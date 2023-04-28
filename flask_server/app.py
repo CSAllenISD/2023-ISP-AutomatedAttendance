c7bda201b73d5f0779a093c5255488e6237adfe1
William Clymire <70544799+Bread21234@users.noreply.github.com>
2023-04-28 00:52:55 -0500

    facial rec within the website

--- flask_server/app.py
+++ flask_server/app.py
@@ -2,6 +2,8 @@ import sqlite3
 from flask import Flask, Response, render_template, request, make_response, url_for, flash, redirect
 import os
 import cv2
+import face_recognition
+import numpy as np
 
 app = Flask(__name__)
 camera = cv2.VideoCapture(0)
@@ -10,9 +12,89 @@ app.config['SECRET_KEY'] = 'your secret key'
 HOST_NAME = "localhost"
 HOST_PORT = 80
 DBFILE = "User1.db"
-# app.debug = True
 
-# (B) HELPER - GET ALL USERS FROM DATABASE
+# Load a sample picture and learn how to recognize it.
+obama_image = face_recognition.load_image_file("faces/obama.jpg")
+obama_face_encoding = face_recognition.face_encodings(obama_image)[0]
+
+# Load a second sample picture and learn how to recognize it.
+william_image = face_recognition.load_image_file("faces/william.jpg")
+william_face_encoding = face_recognition.face_encodings(william_image)[0]
+
+# Create arrays of known face encodings and their names
+known_face_encodings = [
+    obama_face_encoding,
+    william_face_encoding
+]
+known_face_names = [
+    "Barack Obama",
+    "William Clymire"
+]
+
+# Initialize some variables
+face_locations = []
+face_encodings = []
+face_names = []
+process_this_frame = True
+
+# webpage camera
+
+
+def gen_frames():
+    while True:
+        success, frame = camera.read()  # read the camera frame
+        if not success:
+            break
+        else:
+            # Resize frame of video to 1/4 size for faster face recognition processing
+            small_frame = cv2.resize(frame, (0, 0), fx=0.25, fy=0.25)
+            # Convert the image from BGR color (which OpenCV uses) to RGB color (which face_recognition uses)
+            rgb_small_frame = small_frame[:, :, ::-1]
+
+            # Only process every other frame of video to save time
+
+            # Find all the faces and face encodings in the current frame of video
+            face_locations = face_recognition.face_locations(rgb_small_frame)
+            face_encodings = face_recognition.face_encodings(
+                rgb_small_frame, face_locations)
+            face_names = []
+            for face_encoding in face_encodings:
+                # See if the face is a match for the known face(s)
+                matches = face_recognition.compare_faces(
+                    known_face_encodings, face_encoding)
+                name = "Unknown"
+                # Or instead, use the known face with the smallest distance to the new face
+                face_distances = face_recognition.face_distance(
+                    known_face_encodings, face_encoding)
+                best_match_index = np.argmin(face_distances)
+                if matches[best_match_index]:
+                    name = known_face_names[best_match_index]
+
+                face_names.append(name)
+
+            # Display the results
+            for (top, right, bottom, left), name in zip(face_locations, face_names):
+                # Scale back up face locations since the frame we detected in was scaled to 1/4 size
+                top *= 4
+                right *= 4
+                bottom *= 4
+                left *= 4
+
+                # Draw a box around the face
+                cv2.rectangle(frame, (left, top),
+                              (right, bottom), (0, 0, 255), 2)
+
+                # Draw a label with a name below the face
+                cv2.rectangle(frame, (left, bottom - 35),
+                              (right, bottom), (0, 0, 255), cv2.FILLED)
+                font = cv2.FONT_HERSHEY_DUPLEX
+                cv2.putText(frame, name, (left + 6, bottom - 6),
+                            font, 1.0, (255, 255, 255), 1)
+
+            ret, buffer = cv2.imencode('.jpg', frame)
+            frame = buffer.tobytes()
+            yield (b'--frame\r\n'
+                   b'Content-Type: image/jpeg\r\n\r\n' + frame + b'\r\n')
 
 
 def getstudents(period):
@@ -22,26 +104,6 @@ def getstudents(period):
     results = cursor.fetchall()
     conn.close()
     return results
-
-# webpage camera
-
-
-def generate_frames():
-    while True:
-
-        # read the cam frame
-        success, frame = camera.read()
-        if not success:
-            break
-        else:
-            # HERE
-
-            #
-            ret, buffer = cv2.imencode('.jpg', frame)
-            frame = buffer.tobytes()
-
-            yield (b'--frame\r\n'
-               b'Content-Type: image/jpeg\r\n\r\n' + frame + b'\r\n')
 
 
 @app.route('/')
@@ -118,13 +180,12 @@ def classes():
 
 @app.route('/video/')
 def video():
-    return Response(generate_frames(), mimetype='multipart/x-mixed-replace; boundary=frame')
+    return Response(gen_frames(), mimetype='multipart/x-mixed-replace; boundary=frame')
 
 
 @app.route('/FAQ/')
 def FAQ():
     return render_template('FAQ.html')
-
 
 if __name__ == '__main__':
     app.run(HOST_NAME, HOST_PORT)
--- flask_server/temp.py
+++ flask_server/temp.py
@@ -15,22 +15,21 @@ import numpy as np
 video_capture = cv2.VideoCapture(0)
 
 # Load a sample picture and learn how to recognize it.
-obama_image = face_recognition.load_image_file("flask_server/faces/obama.jpg")
-obama_face_encoding = face_recognition.face_encodings(obama_image)[0]
+krish_image = face_recognition.load_image_file("flask_server/faces/obama.jpg")
+krish_face_encoding = face_recognition.face_encodings(krish_image)[0]
 
 # Load a second sample picture and learn how to recognize it.
-william_image = face_recognition.load_image_file(
-    "flask_server/faces/william.jpg")
-william_face_encoding = face_recognition.face_encodings(william_image)[0]
+bradley_image = face_recognition.load_image_file("flask_server/faces/William.jpg")
+bradley_face_encoding = face_recognition.face_encodings(bradley_image)[0]
 
 # Create arrays of known face encodings and their names
 known_face_encodings = [
-    obama_face_encoding,
-    william_face_encoding
+    krish_face_encoding,
+    bradley_face_encoding
 ]
 known_face_names = [
-    "Barack Obama",
-    "William Clymire"
+    "Krish",
+    "Bradley"
 ]
 
 # Initialize some variables
@@ -43,24 +42,22 @@ while True:
     # Grab a single frame of video
     ret, frame = video_capture.read()
 
+    # Resize frame of video to 1/4 size for faster face recognition processing
+    small_frame = cv2.resize(frame, (0, 0), fx=0.25, fy=0.25)
+
+    # Convert the image from BGR color (which OpenCV uses) to RGB color (which face_recognition uses)
+    rgb_small_frame = small_frame[:, :, ::-1]
+
     # Only process every other frame of video to save time
     if process_this_frame:
-        # Resize frame of video to 1/4 size for faster face recognition processing
-        small_frame = cv2.resize(frame, (0, 0), fx=0.25, fy=0.25)
-
-        # Convert the image from BGR color (which OpenCV uses) to RGB color (which face_recognition uses)
-        rgb_small_frame = small_frame[:, :, ::-1]
-
         # Find all the faces and face encodings in the current frame of video
         face_locations = face_recognition.face_locations(rgb_small_frame)
-        face_encodings = face_recognition.face_encodings(
-            rgb_small_frame, face_locations)
+        face_encodings = face_recognition.face_encodings(rgb_small_frame, face_locations)
 
         face_names = []
         for face_encoding in face_encodings:
             # See if the face is a match for the known face(s)
-            matches = face_recognition.compare_faces(
-                known_face_encodings, face_encoding)
+            matches = face_recognition.compare_faces(known_face_encodings, face_encoding)
             name = "Unknown"
 
             # # If a match was found in known_face_encodings, just use the first one.
@@ -69,8 +66,7 @@ while True:
             #     name = known_face_names[first_match_index]
 
             # Or instead, use the known face with the smallest distance to the new face
-            face_distances = face_recognition.face_distance(
-                known_face_encodings, face_encoding)
+            face_distances = face_recognition.face_distance(known_face_encodings, face_encoding)
             best_match_index = np.argmin(face_distances)
             if matches[best_match_index]:
                 name = known_face_names[best_match_index]
@@ -78,6 +74,7 @@ while True:
             face_names.append(name)
 
     process_this_frame = not process_this_frame
+
 
     # Display the results
     for (top, right, bottom, left), name in zip(face_locations, face_names):
@@ -91,11 +88,9 @@ while True:
         cv2.rectangle(frame, (left, top), (right, bottom), (0, 0, 255), 2)
 
         # Draw a label with a name below the face
-        cv2.rectangle(frame, (left, bottom - 35),
-                      (right, bottom), (0, 0, 255), cv2.FILLED)
+        cv2.rectangle(frame, (left, bottom - 35), (right, bottom), (0, 0, 255), cv2.FILLED)
         font = cv2.FONT_HERSHEY_DUPLEX
-        cv2.putText(frame, name, (left + 6, bottom - 6),
-                    font, 1.0, (255, 255, 255), 1)
+        cv2.putText(frame, name, (left + 6, bottom - 6), font, 1.0, (255, 255, 255), 1)
 
     # Display the resulting image
     cv2.imshow('Video', frame)
