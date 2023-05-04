21f9e603087a92f277afa72a532319e99349bdce
AkshayVemulapalli <akshay.vemulapalli@student.allenisd.org>
2023-05-03 20:34:23 -0500

    updated the user class to include a method to create a database

--- User.py
+++ User.py
@@ -20,4 +20,20 @@ class User:
         self.passwordhash = passwordhash
         self.classes = classes
 
-user = User("John Doe", "johndoe@example.com", "123456789", "1a2b3c4d5e6f7a8b9c0d1e2f3a4b5c6d", ["Period 2", "Period 3", "Period 4"])
+    def create_database(self):
+        db_filename = str(self.ID) + ".db"
+        conn = sqlite3.connect(db_filename)
+        c = conn.cursor()
+
+        # Create a table for each class period
+        for period in self.classes:
+            table_name = "period_" + str(period).replace(" ", "_")
+            c.execute(f"CREATE TABLE IF NOT EXISTS {table_name} (stu_id INTEGER NOT NULL, stu_name TEXT NOT NULL, stu_attendance TEXT NOT NULL)")
+
+        conn.commit()
+        conn.close()
+        print("Database and tables created successfully.")
+
+
+
+exUser = User("John Doe", "johndoe@example.com", "123456789", "1a2b3c4d5e6f7a8b9c0d1e2f3a4b5c6d", ["Period 2", "Period 3", "Period 4"])
