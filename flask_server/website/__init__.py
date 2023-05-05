cb34df9cc6a312f1e52e6e043cb9f7661f49ab93
William Clymire <70544799+Bread21234@users.noreply.github.com>
2023-05-05 00:49:15 -0500

    updates every few seconds

--- flask_server/website/__init__.py
+++ flask_server/website/__init__.py
@@ -17,15 +17,15 @@ DBFILE = "User1.db"
 
 def create_app():
     app = Flask(__name__)
-    app.config['SECRET_KEY'] = 'a0s98fhawepjif-a9s8jdf'
-    app.config['SQLALCHEMY_DATABASE_URI'] = f'sqlite:///{DBFILE}'
+    app.config["SECRET_KEY"] = "a0s98fhawepjif-a9s8jdf"
+    app.config["SQLALCHEMY_DATABASE_URI"] = f"sqlite:///{DBFILE}"
     db.init_app(app)
 
     from .views import views
     from .auth import auth
 
-    app.register_blueprint(views, url_prefix='/')
-    app.register_blueprint(auth, url_prefix='/')
+    app.register_blueprint(views, url_prefix="/")
+    app.register_blueprint(auth, url_prefix="/")
 
     from .models import User
 
@@ -33,7 +33,7 @@ def create_app():
         db.create_all()
 
     login_manager = LoginManager()
-    login_manager.login_view = 'auth.login'
+    login_manager.login_view = "auth.login"
     login_manager.init_app(app)
 
     @login_manager.user_loader
@@ -50,14 +50,15 @@ def create_app():
 
 
 def getstudents(period):
-  conn = sqlite3.connect(DBFILE)
-  cursor = conn.cursor()
-  cursor.execute("SELECT * FROM " + period)
-  results = cursor.fetchall()
-  conn.close()
-  return results
+    conn = sqlite3.connect(DBFILE)
+    cursor = conn.cursor()
+    cursor.execute("SELECT * FROM " + period)
+    results = cursor.fetchall()
+    conn.close()
+    return results
 
-def update_attendance(period,stu_name):
+
+def update_attendance(period, stu_name):
     # Connect to the database
     conn = sqlite3.connect(DBFILE)
     c = conn.cursor()
@@ -65,16 +66,19 @@ def update_attendance(period,stu_name):
     # get the current period
     c.execute("SELECT * FROM " + period)
     results = c.fetchall()
-    
+
     # Update attendance
-    c.execute("UPDATE " + period + " SET stu_attendance = ? WHERE stu_name= ?", ("Present", stu_name))
+    c.execute(
+        "UPDATE " + period + " SET stu_attendance = ? WHERE stu_name= ?",
+        ("Present", stu_name),
+    )
     conn.commit()
     c.close()
     conn.close()
 
 
 # camera
-# 
+#
 # FACE REC---------------------
 camera = cv2.VideoCapture(0)
 obama_image = face_recognition.load_image_file("website/faces/obama.jpg")
@@ -89,22 +93,15 @@ izzy_face_encoding = face_recognition.face_encodings(izzy_image)[0]
 
 
 # Create arrays of known face encodings and their names
-known_face_encodings = [
-    obama_face_encoding,
-    william_face_encoding,
-    izzy_face_encoding
-]
-known_face_names = [
-    "Barack Obama",
-    "William Clymire",
-    "Isabelle Holden"
-]
+known_face_encodings = [obama_face_encoding, william_face_encoding, izzy_face_encoding]
+known_face_names = ["Barack Obama", "William Clymire", "Isabelle Holden"]
 
 # Initialize some variables
 face_locations = []
 face_encodings = []
 face_names = []
 process_this_frame = True
+
 
 def gen_frames(period):
     while True:
@@ -122,22 +119,25 @@ def gen_frames(period):
             # Find all the faces and face encodings in the current frame of video
             face_locations = face_recognition.face_locations(rgb_small_frame)
             face_encodings = face_recognition.face_encodings(
-                rgb_small_frame, face_locations)
+                rgb_small_frame, face_locations
+            )
             face_names = []
             for face_encoding in face_encodings:
                 # See if the face is a match for the known face(s)
                 matches = face_recognition.compare_faces(
-                    known_face_encodings, face_encoding)
+                    known_face_encodings, face_encoding
+                )
                 name = "Unknown"
                 # Or instead, use the known face with the smallest distance to the new face
                 face_distances = face_recognition.face_distance(
-                    known_face_encodings, face_encoding)
+                    known_face_encodings, face_encoding
+                )
                 best_match_index = np.argmin(face_distances)
                 if matches[best_match_index]:
                     name = known_face_names[best_match_index]
 
                 face_names.append(name)
-                update_attendance(period,name)
+                update_attendance(period, name)
 
             # Display the results
             for (top, right, bottom, left), name in zip(face_locations, face_names):
@@ -148,17 +148,17 @@ def gen_frames(period):
                 left *= 4
 
                 # Draw a box around the face
-                cv2.rectangle(frame, (left, top),
-                              (right, bottom), (0, 0, 255), 2)
+                cv2.rectangle(frame, (left, top), (right, bottom), (0, 0, 255), 2)
 
                 # Draw a label with a name below the face
-                cv2.rectangle(frame, (left, bottom - 35),
-                              (right, bottom), (0, 0, 255), cv2.FILLED)
+                cv2.rectangle(
+                    frame, (left, bottom - 35), (right, bottom), (0, 0, 255), cv2.FILLED
+                )
                 font = cv2.FONT_HERSHEY_DUPLEX
-                cv2.putText(frame, name, (left + 6, bottom - 6),
-                            font, 1.0, (255, 255, 255), 1)
+                cv2.putText(
+                    frame, name, (left + 6, bottom - 6), font, 1.0, (255, 255, 255), 1
+                )
 
-            ret, buffer = cv2.imencode('.jpg', frame)
+            ret, buffer = cv2.imencode(".jpg", frame)
             frame = buffer.tobytes()
-            yield (b'--frame\r\n'
-                   b'Content-Type: image/jpeg\r\n\r\n' + frame + b'\r\n')
+            yield (b"--frame\r\n" b"Content-Type: image/jpeg\r\n\r\n" + frame + b"\r\n")
--- flask_server/website/views.py
+++ flask_server/website/views.py
@@ -4,123 +4,139 @@ from . import db, HOST_NAME, HOST_PORT, gen_frames, getstudents
 import datetime
 import time
 
-views = Blueprint('views', __name__)
+views = Blueprint("views", __name__)
 
 
-@views.route('/', methods=['GET', 'POST'])
+@views.route("/", methods=["GET", "POST"])
 def landing_page():
     return render_template("landing.html")
 
 
-@views.route('/dashboard/', methods=['GET', 'POST'])
+@views.route("/dashboard/", methods=["GET", "POST"])
 @login_required
 def dashboard():
     return render_template("dashboard.html", user=current_user)
 
-@views.route('/P1/')
+
+@views.route("/P1/")
 def P1():
-  # (C1) GET ALL students
-    students = getstudents('Period1')
+    # (C1) GET ALL students
+    students = getstudents("Period1")
     time.sleep(3.0)
- # (C2) RENDER HTML PAGE
+    # (C2) RENDER HTML PAGE
     return render_template("dashboardTable.html", student=students)
 
-@views.route('/P2/')
+
+@views.route("/P2/")
 def P2():
-  # (C1) GET ALL students
-    students = getstudents('Period2')
+    # (C1) GET ALL students
+    students = getstudents("Period2")
     time.sleep(3.0)
- # (C2) RENDER HTML PAGE
+    # (C2) RENDER HTML PAGE
     return render_template("dashboardTable.html", student=students)
 
-@views.route('/P3/')
+
+@views.route("/P3/")
 def P3():
-  # (C1) GET ALL students
-    students = getstudents('Period3')
+    # (C1) GET ALL students
+    students = getstudents("Period3")
     time.sleep(3.0)
- # (C2) RENDER HTML PAGE
+    # (C2) RENDER HTML PAGE
     return render_template("dashboardTable.html", student=students)
 
-@views.route('/P4/')
+
+@views.route("/P4/")
 def P4():
-  # (C1) GET ALL students
-    students = getstudents('Period4')
+    # (C1) GET ALL students
+    students = getstudents("Period4")
     time.sleep(3.0)
- # (C2) RENDER HTML PAGE
+    # (C2) RENDER HTML PAGE
     return render_template("dashboardTable.html", student=students)
 
-@views.route('/P8/')
+
+@views.route("/P8/")
 def P5():
-  # (C1) GET ALL students
-    students = getstudents('Period8')
+    # (C1) GET ALL students
+    students = getstudents("Period8")
     time.sleep(3.0)
- # (C2) RENDER HTML PAGE
+    # (C2) RENDER HTML PAGE
     return render_template("dashboardTable.html", student=students)
 
-@views.route('/add-student/')
+
+@views.route("/add-student/")
 def add_student():
-    return render_template('add-student.html')
+    return render_template("add-student.html")
 
-@views.route('/classes/')
+
+@views.route("/classes/")
 def classes():
-    return render_template('classes.html')
-
-#@app.route('/video/')
-#def video():
- #   return Response(generate_frames(),mimetype='multipart/x-mixed-replace; boundary=frame')
+    return render_template("classes.html")
 
 
-@views.route('/classes/')
+# @app.route('/video/')
+# def video():
+#   return Response(generate_frames(),mimetype='multipart/x-mixed-replace; boundary=frame')
+
+
+@views.route("/classes/")
 def FAQ():
-    return render_template('FAQ.html')
-  
-@views.route('/AttendanceRep/')
-def AttendanceReport():
-    return render_template('AttendanceReport.html')
-    
-@views.route('/Help/')
-def HelpSupport():
-    return render_template('HelpSupport.html')
-    
-@views.route('/Student-Info/')
-def StudentInformation():
-    return render_template('Student-Information.html')
-    
-@views.route('/base/')
-def base():
-    return render_template('base.html')
-    
-@views.route('/sign-in/')
-def layout():
-    return render_template('layout.html')
-    
+    return render_template("FAQ.html")
 
-@views.route('/periods/')
+
+@views.route("/AttendanceRep/")
+def AttendanceReport():
+    return render_template("AttendanceReport.html")
+
+
+@views.route("/Help/")
+def HelpSupport():
+    return render_template("HelpSupport.html")
+
+
+@views.route("/Student-Info/")
+def StudentInformation():
+    return render_template("Student-Information.html")
+
+
+@views.route("/base/")
+def base():
+    return render_template("base.html")
+
+
+@views.route("/sign-in/")
+def layout():
+    return render_template("layout.html")
+
+
+@views.route("/periods/")
 def periods():
-    return render_template('period-display.html')
-    
-@views.route('/temp-period/')
+    return render_template("period-display.html")
+
+
+@views.route("/temp-period/")
 def temp():
-    return render_template('temp.html')
+    return render_template("temp.html")
+
 
 # overhaul time funcs
-@views.route('/video/') 
+@views.route("/video/")
 def video():
-    period = 'Period1'
-    # now = datetime.datetime.now().time()
-    # if (now.hour == 8 and 30 <= now.minute) or (now.hour == 9 and now.minute < 45):
-    #     period = 'Period1'
-    # elif (now.hour == 9 and 45 <= now.minute) or (now.hour == 11 and now.minute < 25):
-    #     period = 'Period2'
-    # elif (now.hour == 11 and 25 <= now.minute) or (now.hour == 1 and now.minute < 30):
-    #     period = 'Period3'
-    # elif (now.hour == 1 and 30 <= now.minute) or (now.hour == 3 and now.minute < 10):
-    #     period = 'Period4'
-    # elif (now.hour == 3 and 10 <= now.minute) or (now.hour == 4 and now.minute < 10):
-    #     period = 'Period8'
-    # else:
-    #     period = 'Period1' #testing purposes 
+    period = "Period1"
+    now = datetime.datetime.now().time()
+    if (now.hour == 8 and 30 <= now.minute) or (now.hour == 9 and now.minute < 45):
+        period = "Period1"
+    elif (now.hour == 9 and 45 <= now.minute) or (now.hour == 11 and now.minute < 25):
+        period = "Period2"
+    elif (now.hour == 11 and 25 <= now.minute) or (now.hour == 1 and now.minute < 30):
+        period = "Period3"
+    elif (now.hour == 1 and 30 <= now.minute) or (now.hour == 3 and now.minute < 10):
+        period = "Period4"
+    elif (now.hour == 3 and 10 <= now.minute) or (now.hour == 4 and now.minute < 10):
+        period = "Period8"
+    else:
+        period = "Period1"  # testing purposes
 
-    return Response(gen_frames(period), mimetype='multipart/x-mixed-replace; boundary=frame')
+    return Response(
+        gen_frames(period), mimetype="multipart/x-mixed-replace; boundary=frame"
+    )
     # return Response(gen_frames(), mimetype='multipart/x-mixed-replace; boundary=frame')
-
