a16344f1c97389275db6bda0c0f220eb1d4a752b
oliver.hankins <oliver.hankins@gmail.com>
2023-05-10 12:02:47 -0500

    merge

--- flask_server/website/__init__.py
+++ flask_server/website/__init__.py
@@ -1,27 +1,32 @@
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
+import os
 from os import path
 from flask_login import LoginManager
 import sqlite3
-
+import cv2
+import face_recognition
+import numpy as np
+import datetime
+import concurrent.futures
 
 db = SQLAlchemy()
 HOST_NAME = "localhost"
 HOST_PORT = 80
-DBFILE = "User1.db"
-
+camera = cv2.VideoCapture(0)
+DBFILE = ("User1.db")
 
 def create_app():
     app = Flask(__name__)
-    app.config['SECRET_KEY'] = 'a0s98fhawepjif-a9s8jdf'
-    app.config['SQLALCHEMY_DATABASE_URI'] = f'sqlite:///{DBFILE}'
+    app.config["SECRET_KEY"] = "a0s98fhawepjif-a9s8jdf"
+    app.config["SQLALCHEMY_DATABASE_URI"] = f"sqlite:///{DBFILE}"
     db.init_app(app)
 
     from .views import views
     from .auth import auth
 
-    app.register_blueprint(views, url_prefix='/')
-    app.register_blueprint(auth, url_prefix='/')
+    app.register_blueprint(views, url_prefix="/")
+    app.register_blueprint(auth, url_prefix="/")
 
     from .models import User, Period
 
@@ -29,7 +34,7 @@ def create_app():
         db.create_all()
 
     login_manager = LoginManager()
-    login_manager.login_view = 'auth.login'
+    login_manager.login_view = "auth.login"
     login_manager.init_app(app)
 
     @login_manager.user_loader
@@ -40,30 +45,181 @@ def create_app():
 
 
 def create_database(app):
-    if not path.exists('website/' + DBFILE):
+    if not path.exists(DBFILE):
         db.create_all(app=app)
-        print('Created Database!')
+        SQLFILE = "allClasses.sql"
+
+    # (C) DELETE OLD DATABASE IF EXIST
+    if os.path.exists(DBFILE):
+        os.remove(DBFILE)
+
+    # (D) IMPORT SQL
+    conn = sqlite3.connect(DBFILE)
+    with open(SQLFILE) as f:
+        conn.executescript(f.read())
+    conn.commit()
+    conn.close()
+    print("Database created!")
 
 
 def getstudents(period):
-  conn = sqlite3.connect(DBFILE)
-  cursor = conn.cursor()
-  cursor.execute("SELECT * FROM " + period)
-  results = cursor.fetchall()
-  conn.close()
-  return results
+    conn = sqlite3.connect(DBFILE)
+    cursor = conn.cursor()
+    cursor.execute("SELECT * FROM " + period)
+    results = cursor.fetchall()
+    conn.close()
+    return results
 
-  # webpage camera
-#def generate_frames():
- #   while True:
+
+def update_attendance(period, stu_name):
+    # Connect to the database
+    conn = sqlite3.connect(DBFILE)
+    c = conn.cursor()
+
+    # get the current period
+    c.execute("SELECT * FROM " + period)
+    results = c.fetchall()
+
+    if isTardy(period) == True:
+        c.execute(
+            "UPDATE " + period + " SET stu_attendance = ? WHERE stu_name= ?",
+            ("Tardy", stu_name),
+        )
+    # Update attendance
+    else:
+        c.execute(
+            "UPDATE " + period + " SET stu_attendance = ? WHERE stu_name= ?",
+            ("Present", stu_name),
+        )
+    conn.commit()
+    c.close()
+    conn.close()
+
+
+def isTardy(period):
+    current = datetime.datetime.now().time()
+    if period == "Period1":
+        start = datetime.time(8, 30, 0)
+        if current > start:
+            return True
+    if period == "Period2":
+        start = datetime.time(9, 45, 0)
+        if current > start:
+            return True
+    if period == "Period3":
+        start = datetime.time(11, 25, 0)
+        if current > start:
+            return True
+    if period == "Period4":
+        start = datetime.time(1, 30, 0)
+        if current > start:
+            return True
+    if period == "Period8":
+        start = datetime.time(3, 10, 0)
+        if current > start:
+            return True
+    else:
+        return False
+
+
+# camera
 #
- #       #read the cam frame
-  #      success, frame=camera.read()
-   #     if not success:
-    #        break
-     #   else:
-      #      ret, buffer=cv2.imencode('.jpg', frame)
-       #     frame=buffer.tobytes()
-        #
-        #yield(b'--frame\r\n'
-         #     b'Content-Type: image/jpeg\r\n\r\n' + frame + b'\r\n')
+# FACE REC---------------------
+obama_image = face_recognition.load_image_file("website/faces/obama.jpg")
+obama_face_encoding = face_recognition.face_encodings(obama_image)[0]
+
+# Load a second sample picture and learn how to recognize it.
+william_image = face_recognition.load_image_file("website/faces/william.jpg")
+william_face_encoding = face_recognition.face_encodings(william_image)[0]
+
+izzy_image = face_recognition.load_image_file("website/faces/izzy.jpg")
+izzy_face_encoding = face_recognition.face_encodings(izzy_image)[0]
+
+
+# Create arrays of known face encodings and their names
+known_face_encodings = [obama_face_encoding, william_face_encoding, izzy_face_encoding]
+known_face_names = ["Barack Obama", "William Clymire", "Isabelle Holden"]
+
+# Initialize some variables
+face_locations = []
+face_encodings = []
+face_names = []
+process_this_frame = True
+
+
+def process_frame(frame, period):
+    # Resize frame of video to 1/4 size for faster face recognition processing
+    small_frame = cv2.resize(frame, (0, 0), fx=0.25, fy=0.25)
+    # Convert the image from BGR color (which OpenCV uses) to RGB color (which face_recognition uses)
+    rgb_small_frame = small_frame[:, :, ::-1]
+
+    # Find all the faces and face encodings in the current frame of video
+    face_locations = face_recognition.face_locations(rgb_small_frame)
+    face_encodings = face_recognition.face_encodings(rgb_small_frame, face_locations)
+    face_names = []
+    for face_encoding in face_encodings:
+        # See if the face is a match for the known face(s)
+        matches = face_recognition.compare_faces(known_face_encodings, face_encoding)
+        name = "Unknown"
+        # Or instead, use the known face with the smallest distance to the new face
+        face_distances = face_recognition.face_distance(
+            known_face_encodings, face_encoding
+        )
+        best_match_index = np.argmin(face_distances)
+        if matches[best_match_index]:
+            name = known_face_names[best_match_index]
+
+        face_names.append(name)
+        update_attendance(period, name)
+
+    return (face_locations, face_names)
+
+
+def gen_frames(period):
+    camera = cv2.VideoCapture(0)
+    try:
+        while True:
+            success, frame = camera.read()
+            if not success:
+                break
+            else:
+                face_locations, face_names = process_frame(frame, period)
+
+                # Display the results
+                for (top, right, bottom, left), name in zip(face_locations, face_names):
+                    # Scale back up face locations since the frame we detected in was scaled to 1/4 size
+                    top *= 4
+                    right *= 4
+                    bottom *= 4
+                    left *= 4
+
+                    # Draw a box around the face
+                    cv2.rectangle(frame, (left, top), (right, bottom), (0, 0, 255), 2)
+
+                    # Draw a label with a name below the face
+                    cv2.rectangle(
+                        frame,
+                        (left, bottom - 35),
+                        (right, bottom),
+                        (0, 0, 255),
+                        cv2.FILLED,
+                    )
+                    font = cv2.FONT_HERSHEY_DUPLEX
+                    cv2.putText(
+                        frame,
+                        name,
+                        (left + 6, bottom - 6),
+                        font,
+                        1.0,
+                        (255, 255, 255),
+                        1,
+                    )
+
+                # Return the frame with the bounding boxes and names
+                ret, buffer = cv2.imencode(".jpg", frame)
+                frame = buffer.tobytes()
+                yield (
+                    b"--frame\r\n" b"Content-Type: image/jpeg\r\n\r\n" + frame + b"\r\n"
+                )
+    finally:
+        camera.release()
--- flask_server/website/views.py
+++ flask_server/website/views.py
@@ -1,62 +1,101 @@
-from flask import Blueprint, render_template, request, flash
+from flask import Blueprint, Response, render_template, request, flash
 from flask_login import login_required, current_user
+<<<<<<< HEAD
 from . import db, HOST_NAME, HOST_PORT, getstudents
 from .models import Period
+=======
+from . import db, HOST_NAME, HOST_PORT, gen_frames, getstudents
+import datetime
+import time
+>>>>>>> 368bab642d665c4fa9e67462812f2dec74414c44
 
-views = Blueprint('views', __name__)
+views = Blueprint("views", __name__)
 
 
-@views.route('/', methods=['GET', 'POST'])
+@views.route("/", methods=["GET", "POST"])
 def landing_page():
     return render_template("landing.html")
 
 
-@views.route('/dashboard/', methods=['GET', 'POST'])
+@views.route("/dashboard/", methods=["GET", "POST"])
 @login_required
 def dashboard():
     return render_template("dashboard.html", user=current_user)
 
+<<<<<<< HEAD
 @views.route('/p1/')
+=======
+
+@views.route("/P1/")
+>>>>>>> 368bab642d665c4fa9e67462812f2dec74414c44
 def P1():
-  # (C1) GET ALL students
-    students = getstudents('Period1')
- # (C2) RENDER HTML PAGE
+    # (C1) GET ALL students
+    students = getstudents("Period1")
+    time.sleep(2.0)
+    # (C2) RENDER HTML PAGE
     return render_template("dashboardTable.html", student=students)
 
+<<<<<<< HEAD
 @views.route('/p2/')
+=======
+
+@views.route("/P2/")
+>>>>>>> 368bab642d665c4fa9e67462812f2dec74414c44
 def P2():
-  # (C1) GET ALL students
-    students = getstudents('Period2')
- # (C2) RENDER HTML PAGE
+    # (C1) GET ALL students
+    students = getstudents("Period2")
+    time.sleep(3.0)
+    # (C2) RENDER HTML PAGE
     return render_template("dashboardTable.html", student=students)
 
+<<<<<<< HEAD
 @views.route('/p3/')
+=======
+
+@views.route("/P3/")
+>>>>>>> 368bab642d665c4fa9e67462812f2dec74414c44
 def P3():
-  # (C1) GET ALL students
-    students = getstudents('Period3')
- # (C2) RENDER HTML PAGE
+    # (C1) GET ALL students
+    students = getstudents("Period3")
+    time.sleep(3.0)
+    # (C2) RENDER HTML PAGE
     return render_template("dashboardTable.html", student=students)
 
+<<<<<<< HEAD
 @views.route('/p4/')
+=======
+
+@views.route("/P4/")
+>>>>>>> 368bab642d665c4fa9e67462812f2dec74414c44
 def P4():
-  # (C1) GET ALL students
-    students = getstudents('Period4')
- # (C2) RENDER HTML PAGE
+    # (C1) GET ALL students
+    students = getstudents("Period4")
+    time.sleep(3.0)
+    # (C2) RENDER HTML PAGE
     return render_template("dashboardTable.html", student=students)
 
+<<<<<<< HEAD
 @views.route('/p5/')
+=======
+
+@views.route("/P8/")
+>>>>>>> 368bab642d665c4fa9e67462812f2dec74414c44
 def P5():
-  # (C1) GET ALL students
-    students = getstudents('Period5')
- # (C2) RENDER HTML PAGE
+    # (C1) GET ALL students
+    students = getstudents("Period8")
+    time.sleep(3.0)
+    # (C2) RENDER HTML PAGE
     return render_template("dashboardTable.html", student=students)
 
-@views.route('/add-student/')
-def add_student():
-    return render_template('add-student.html')
 
-@views.route('/classes/')
+@views.route("/add-student/")
+def add_student():
+    return render_template("add-student.html")
+
+
+@views.route("/classes/")
 def classes():
+<<<<<<< HEAD
     if request.method == 'POST': 
         period = request.form.get('period')#Gets the periods from the database
         
@@ -71,10 +110,19 @@ def classes():
 #@app.route('/video/')
 #def video():
  #   return Response(generate_frames(),mimetype='multipart/x-mixed-replace; boundary=frame')
+=======
+    return render_template("classes.html")
+>>>>>>> 368bab642d665c4fa9e67462812f2dec74414c44
 
 
-@views.route('/classes/')
+# @app.route('/video/')
+# def video():
+#   return Response(generate_frames(),mimetype='multipart/x-mixed-replace; boundary=frame')
+
+
+@views.route("/classes/")
 def FAQ():
+<<<<<<< HEAD
     return render_template('FAQ.html')
   
 @views.route('/attendance-rep/')
@@ -97,11 +145,64 @@ def base():
 def layout():
     return render_template('layout.html')
     
+=======
+    return render_template("FAQ.html")
+>>>>>>> 368bab642d665c4fa9e67462812f2dec74414c44
 
-@views.route('/periods/')
+
+@views.route("/AttendanceRep/")
+def AttendanceReport():
+    return render_template("AttendanceReport.html")
+
+
+@views.route("/Help/")
+def HelpSupport():
+    return render_template("HelpSupport.html")
+
+
+@views.route("/Student-Info/")
+def StudentInformation():
+    return render_template("Student-Information.html")
+
+
+@views.route("/base/")
+def base():
+    return render_template("base.html")
+
+
+@views.route("/sign-in/")
+def layout():
+    return render_template("layout.html")
+
+
+@views.route("/periods/")
 def periods():
-    return render_template('period-display.html')
-    
-@views.route('/temp-period/')
+    return render_template("period-display1.html")
+
+
+@views.route("/temp-period/")
 def temp():
-    return render_template('temp.html')
+    return render_template("temp.html")
+
+
+# overhaul time funcs
+@views.route("/video/")
+def video():
+    period = "Period1"
+    # now = datetime.datetime.now().time()
+    # if (now.hour == 8 and 30 <= now.minute) or (now.hour == 9 and now.minute < 45):
+    #     period = "Period1"
+    # elif (now.hour == 9 and 45 <= now.minute) or (now.hour == 11 and now.minute < 25):
+    #     period = "Period2"
+    # elif (now.hour == 11 and 25 <= now.minute) or (now.hour == 1 and now.minute < 30):
+    #     period = "Period3"
+    # elif (now.hour == 1 and 30 <= now.minute) or (now.hour == 3 and now.minute < 10):
+    #     period = "Period4"
+    # elif (now.hour == 3 and 10 <= now.minute) or (now.hour == 4 and now.minute < 10):
+    #     period = "Period8"
+    # else:
+    #     period = "Period1"  # testing purposes
+
+    return Response(
+        gen_frames(period), mimetype="multipart/x-mixed-replace; boundary=frame"
+    )
